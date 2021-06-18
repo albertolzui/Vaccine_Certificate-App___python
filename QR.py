2769657c2b23a83696049fa91cf523e06502a91b
Albert Olanipekun <alaba.olanipekun@stud.th-deg.de>
2021-06-18 19:48:05 +0100

    Renamed methods in class QrScan.

--- QR.py
+++ QR.py
@@ -2,17 +2,17 @@ class QrScan:
     source = "this_vac_info.txt"
     destination = "this_vac_info2.txt"
 
-    def fetch_details(self):
+    def capture_qr(self):
         origin_qr = open(self.source, 'rt')
         self.origin_qr_decrypted = origin_qr.read()
         origin_qr.close()
 
-    def store_details(self):
+    def process_qr(self):
         destination_qr = open(self.destination, 'wt')
         destination_qr.write(self.origin_qr_decrypted)
         destination_qr.close()
 
-    def scan_now(self):
+    def verify_qr_scan(self):
         origin = open(self.source, 'rt')
         origin_reader = origin.read()
         origin.close()
@@ -27,9 +27,9 @@ class QrScan:
 
     @staticmethod
     def execute_qr_scan():
-        QrScan.fetch_details(QrScan)
-        QrScan.store_details(QrScan)
-        QrScan.scan_now(QrScan)
+        QrScan.capture_qr(QrScan)
+        QrScan.process_qr(QrScan)
+        QrScan.verify_qr_scan(QrScan)
 
 
 QrScan.execute_qr_scan()
